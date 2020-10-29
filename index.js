876496bb217c54748c335ab11f4458400c870618
trevismcgill <trevismcgill25@gmail.com>
2020-10-29 17:17:36 -0500

    commit tree 69322794854bbcd958628b6608d49690a718aa9d

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-animal = ""
+let animal = ""
 
 function myAnimal() {
+  animal = "dog"
   return animal
 }
 
 function yourAnimal() {
+  animal = "cat"
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,8 +14,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  let two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
+  
 }
